8e490c636109f0f95dfbffa0b1c2ceca5f2f62d4
Juan Rafael <juanrafaelxpo@outlook.com>
2022-02-25 18:37:28 -0300

    Configuração do bando de dados

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 
-export default (
+const endpointLogin = (
     req : NextApiRequest,
-    res : NextApiResponse
+    res : NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
         if(login === 'admin@admin.com' &&
             senha === 'admin123'){
-            res.status(200).json({msg : 'Usuário logado em sucesso.'});
+            return res.status(200).json({msg : 'Usuário logado em sucesso.'});
         }
         return res.status(405).json({erro : 'Usuário não encontrado.'});
     }
     return res.status(405).json({erro : 'O método informado não é valido.'});
 }
+
+export default conectarMongoDB(endpointLogin);
