d81da0ec52d48303019e4bd4b715d92725bbff55
Juan Rafael <juanrafaelxpo@outlook.com>
2022-02-25 22:51:52 -0300

    Criação de cadastro e autenticação

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,21 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import md5 from "md5";
+import { UsuarioModel } from "../../models/UsuarioModel";
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest,
     res : NextApiResponse<RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' &&
-            senha === 'admin123'){
-            return res.status(200).json({msg : 'Usuário logado em sucesso.'});
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0];
+            return res.status(200).json({msg : `Usuário ${usuarioEncontrado.nome} autenticado em sucesso`});
         }
-        return res.status(405).json({erro : 'Usuário não encontrado.'});
+        return res.status(405).json({erro : 'Usuário não encontrado'});
     }
-    return res.status(405).json({erro : 'O método informado não é valido.'});
+    return res.status(405).json({erro : 'O método informado não é valido'});
 }
 
 export default conectarMongoDB(endpointLogin);
