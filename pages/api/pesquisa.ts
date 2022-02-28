5026e2812001c81da9f19f965afcaac00f0a5f56
Juan Rafael <juanrafaelxpo@outlook.com>
2022-02-27 21:08:32 -0300

    Home de um usuário especifico

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -6,22 +6,29 @@ import { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 
 const pesquisaEndpoint
     = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
-
     try{
         if(req.method ==='GET'){
-            const {filtro} = req.query;
-            if(!filtro || filtro.length < 2){
-                return res.status(400).json({erro : 'Por favor, informar no mínimo 2 caracteres para busca'})
+            if(req?.query?.id){
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+                if(!usuarioEncontrado){
+                    return res.status(400).json({erro : 'Usuário não encontrado'})
+                }
+                usuarioEncontrado.senha = null;
+                return res.status(200).json(usuarioEncontrado);
+            }else{
+                const {filtro} = req.query;
+                if(!filtro || filtro.length < 2){
+                    return res.status(400).json({erro : 'Por favor, informar no mínimo 2 caracteres para busca'})
+                }
+    
+                const usuariosEncontrados = await UsuarioModel.find({
+                    $or: [{nome : {$regex : filtro, $options: 'i'}},
+                        {email : {$regex : filtro, $options: 'i'}}]
+                });
+                return res.status(200).json(usuariosEncontrados);
             }
-
-            const usuariosEncontrados = await UsuarioModel.find({
-                $or: [{nome : {$regex : filtro, $options: 'i'}},
-                    //{email : {$regex : filtro, $options: 'i'}}]
-            });
-
-            return res.status(200).json(usuariosEncontrados);
+            return res.status(405).json({erro : 'Método informado não é válido'})
         }
-        return res.status(405).json({erro : 'Método informado não é válido'})
     }catch(e){
         console.log(e);
         return res.status(500).json({erro : 'Não foi possível buscar usuários: ' + e})
